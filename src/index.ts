37f28b9a25e2658e1cd038dfdacfe17592769051
Alex Nepsha <nepsha1986@gmail.com>
2024-08-08 20:49:00 +0300

    npm registry test

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,7 @@ class Alfred {
       "Please provide post title",
     );
 
-    console.log('New post created successfully!')
+    console.log('New post created successfully! NPM registry test!')
   }
 
   async startDialog() {
